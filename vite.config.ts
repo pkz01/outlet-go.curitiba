79822dec35ec438b8c80f7c1847ed828b6423ed8
pkz01 <mkkcanaloficial2@gmail.com>
2026-09-05 14:33:07 -0300

    Update Vite base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/outlet-go.curitiba/',
   plugins: [react()],
   resolve: {
     alias: {
